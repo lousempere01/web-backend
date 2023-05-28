3211075e5ddb4e5b56c5b14462ad485e7f81b2c6
Lou Sempere <lousempere@gmail.com>
2023-05-28 15:30:03 +0200

    c^c ^p pq:		:pq :qp A23 mfsqmfsqdsf
    fdfsddsds

--- routes/categories.js
+++ routes/categories.js
@@ -1,16 +1,15 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-const auth = require('../middleware/auth');
-const multer = require('../middleware/multer-config');
+const auth = require("../middleware/auth");
+const multer = require("../middleware/multer-config");
 
-const CatCtrl = require('../controllers/categories');
+const CatCtrl = require("../controllers/categories");
 
-router.post('/', auth, multer, CatCtrl.createCategory);
-router.put('/:id', auth, CatCtrl.modifyCategory);
-router.delete('/:id', auth, CatCtrl.deleteCategory);
-router.get('/:id', auth, CatCtrl.getOneCategory);
-router.get('/', auth, CatCtrl.getAllCategories);
+router.post("/", auth, multer, CatCtrl.createCategory);
+router.put("/:id", auth, CatCtrl.modifyCategory);
+router.delete("/:id", auth, CatCtrl.deleteCategory);
+router.get("/:id", auth, CatCtrl.getOneCategory);
+router.get("/", CatCtrl.getAllCategories);
 
 module.exports = router;
-
