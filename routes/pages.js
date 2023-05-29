9fb4e730d035a5c74d9c5d28d7db068a0d90665d
Lou Sempere <lousempere@gmail.com>
2023-05-30 00:46:27 +0200

    didn't work witg user

--- routes/pages.js
+++ routes/pages.js
@@ -6,7 +6,7 @@ const multer = require("../middleware/multer-config");
 
 const pagesCtrl = require("../controllers/pages");
 
-router.post("/", auth, multer, pagesCtrl.createPage);
+router.post("/", multer, pagesCtrl.createPage);
 router.put("/:id", auth, pagesCtrl.modifyPage);
 router.delete("/:id", auth, pagesCtrl.deletePage);
 router.get("/:id", auth, pagesCtrl.getOnePage);
